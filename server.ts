b0726735d377f2c7c37415dbe60661d5c19f0b3c
SirYazoo <rioaurelio25@gmail.com>
2026-06-10 15:22:34 +0700

    chore: initialize TypeScript Express skeleton and PostgreSQL docker container

---
+++ server.ts
@@ -0,0 +1,7 @@
+import "dotenv/config";
+import app from "./src/app";
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "API is operational" });
+});
+
+export default app;
